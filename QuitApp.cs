5b193a90fccde4072102f3a9c86514eab29c77fe
KZJackson <84821778+KZJackson@users.noreply.github.com>
2021-05-27 10:16:45 +1200

    Add files via upload

---
+++ QuitApp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+    /*This script simply contains the function the quit button will execute. 
+    The function is called from the button component on the exit button.
+    The script is simply a source for the function  */
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Game has quit");
+    }
+}
