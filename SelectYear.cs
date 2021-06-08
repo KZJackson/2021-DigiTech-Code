322d6a8a002705e1b3f89b9b5498e16201fef88e
KZJackson <84821778+KZJackson@users.noreply.github.com>
2021-06-08 14:47:16 +1200

    Add files via upload

---
+++ SelectYear.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectYear : MonoBehaviour
+{   //This was my original method, using buttons, for the player to select their year level
+    // I have since changed to using an input field to meet the assessment standard
+    public int YearLevel;
+    
+
+    //The below functions are attached to their corresponding buttons in-game, and are called when their respective button is clicked
+    public void PickYear1(){
+        YearLevel = 1;
+        SceneManager.LoadScene("DiffSelect",LoadSceneMode.Single);
+        Debug.Log("Difficulty Selection Screen Loaded. Year level set to " + YearLevel);
+        
+    }
+
+    public void PickYear2(){
+        YearLevel = 2;
+        SceneManager.LoadScene("DiffSelect",LoadSceneMode.Single);
+        Debug.Log("Difficulty Selection Screen Loaded. Year level set to " + YearLevel);
+    }
+
+    public void PickYear3(){
+        YearLevel = 3;
+        SceneManager.LoadScene("DiffSelect",LoadSceneMode.Single);
+        Debug.Log("Difficulty Selection Screen Loaded. Year level set to " + YearLevel);
+    }
+
+    public void PickYear4(){
+        YearLevel = 4;
+        SceneManager.LoadScene("DiffSelect",LoadSceneMode.Single);
+        Debug.Log("Difficulty Selection Screen Loaded. Year level set to " + YearLevel);
+    }
+    public void PickYear5(){
+        YearLevel = 5;
+        SceneManager.LoadScene("DiffSelect",LoadSceneMode.Single);
+        Debug.Log("Difficulty Selection Screen Loaded. Year level set to " + YearLevel);
+    }
+
+    public void PickYear6(){
+        YearLevel = 6;
+        SceneManager.LoadScene("DiffSelect",LoadSceneMode.Single);
+        Debug.Log("Difficulty Selection Screen Loaded. Year level set to " + YearLevel);
+    }
+
+
+
+
+    //Sets the year level back to zero, which is effectively no value
+
+    public void ResetYearLevel(){
+        YearLevel = 0;
+        Debug.Log("Year Level Reset");
+    }
+}
