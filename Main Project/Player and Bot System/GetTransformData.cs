19b15f2c212d23120e989788a3942893b96f9fe9
KZJackson <84821778+KZJackson@users.noreply.github.com>
2021-08-22 10:24:31 +1200

    Add files via upload

---
+++ Main Project/Player and Bot System/GetTransformData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetTransformData : MonoBehaviour
+{
+    // A script to get Transform Data of the board tiles and adds them to an array to be used by other scripts
+    public Transform[] DataArray;
+    public int length;
+
+
+    void Awake()
+    {
+        DataArray = new Transform[transform.childCount];
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            DataArray[i] = transform.GetChild(i);
+        }
+        length = DataArray.Length;
+    }
+
+}
