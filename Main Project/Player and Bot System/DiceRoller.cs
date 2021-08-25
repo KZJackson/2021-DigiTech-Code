f9fe59bb118501dbf9b057848e6d694e4690aed4
KZJackson <84821778+KZJackson@users.noreply.github.com>
2021-08-25 12:35:35 +1200

    Add files via upload

--- Main Project/Player and Bot System/DiceRoller.cs
+++ Main Project/Player and Bot System/DiceRoller.cs
@@ -20,7 +20,9 @@ public class DiceRoller : MonoBehaviour
     public Vector2 OffScreenPos; //A coordinate set for a position off of the canvas
     public int RollCombination; //A variable to contain the dice combination number
     public int NumberRolled; //A variable to contain the number that was rolled
+    public int[] Rolls = {2,3,4,5,6,7,3,4,5,6,7,8,4,5,6,7,8,9,5,6,7,8,9,10,6,7,8,9,10,11,7,8,9,10,11,12};
     public bool ShouldMove; //A bool to tell other script/s when to move a token
+    
     
     
     //Hides and resets the Dice Roll screen
@@ -89,62 +91,7 @@ public class DiceRoller : MonoBehaviour
         RollDiceButton.GetComponent<RectTransform>().localPosition = OffScreenPos;
         RollCombination = Random.Range(0,35);
         VideoPlayer.clip = Clips[RollCombination];
-        
-        if(RollCombination == 0)
-        {
-            NumberRolled = 2;
-        }
-
-        else if(RollCombination == 1 || RollCombination == 6 )
-        {
-            NumberRolled = 3;
-        }
-
-        else if(RollCombination == 2 || RollCombination == 7 || RollCombination == 12)
-        {
-            NumberRolled = 4;
-        }
-
-        else if(RollCombination == 3 || RollCombination == 8 || RollCombination == 13 || RollCombination == 18)
-        {
-            NumberRolled = 5;
-        }
-
-        else if(RollCombination == 4 || RollCombination == 9 || RollCombination == 14 || RollCombination == 19 || RollCombination == 24)
-        {
-            NumberRolled = 6;
-        }
-
-        else if(RollCombination == 5 || RollCombination == 10 || RollCombination == 15 || RollCombination == 20 || RollCombination == 25 || RollCombination == 30)
-        {
-            NumberRolled = 7;
-        }
-
-        else if(RollCombination == 11 || RollCombination == 16 || RollCombination == 21 || RollCombination == 26 || RollCombination == 31)
-        {
-            NumberRolled = 8;
-        }
-
-        else if(RollCombination == 17 || RollCombination == 22 || RollCombination == 27 || RollCombination == 32)
-        {
-            NumberRolled = 9;
-        }
-
-        else if(RollCombination == 23 || RollCombination == 28 || RollCombination == 33)
-        {
-            NumberRolled = 10;
-        }
-
-        else if(RollCombination == 29 || RollCombination == 34 )
-        {
-            NumberRolled = 11;
-        }
-
-        else if(RollCombination == 35)
-        {
-            NumberRolled = 12;
-        }
-        
+        NumberRolled = Rolls[RollCombination];
         VideoPlayer.Play();
         StartCoroutine(WaitTimer());
         VideoObject.texture = VideoTexture;
