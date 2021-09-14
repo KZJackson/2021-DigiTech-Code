6af31001dc9dd235faaebc24991329ad6c9886fd
KZJackson <84821778+KZJackson@users.noreply.github.com>
2021-09-14 15:41:04 +1200

    Add files via upload

---
+++ Main Project/Game Setup/BackgroundSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSystem : MonoBehaviour
+{
+    public Shader[] Shaders;
+    public Renderer Renderer;
+
+
+    public IEnumerator ChangeShader()
+    {
+        yield return new WaitForSeconds(5);
+        Renderer.material.shader = Shaders[Random.Range(0,6)];
+        StartCoroutine(ChangeShader());
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer = GetComponent<Renderer>();
+        Renderer.material.shader = Shaders[Random.Range(0,6)];
+        StartCoroutine(ChangeShader());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
